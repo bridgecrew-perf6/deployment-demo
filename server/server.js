9376eaa221257a7ef99a5e1a90e1f10602392102
Shelby Webb <shelbylyn.webb@gmail.com>
2022-06-20 17:54:11 -0600

    added css

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { rmSync } = require("fs");
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
